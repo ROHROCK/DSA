a5204b76a8abe338b15e83212e07146d644239df
Rohit <rohitsa40@gmail.com>
2017-11-03 17:42:42 +0530

    decreased the code complexity

--- polynomial.cpp
+++ polynomial.cpp
@@ -1,133 +1,105 @@
+//prg to realize polynomial eq
 #include<iostream>
+#include<stdlib.h>
 #include<math.h>
-using namespace std;
-
-struct p{
-	int co;
-	int power;
-};
+#define MAX 20
+using namespace std ;
 
 class poly{
-	 public:
-        int deg;
-        p arr[10];
-       
-        poly()
-        {
-        			deg=0;
-        			for (int i = 0; i < 10; ++i)
-        			{
-        				arr[10].co= 0;
-        				arr[10].power=0;
-        			}
-        }
-        friend istream & operator >>(istream &input , poly &p)
-        {
-                		cout<<"enter the degree value: "<<endl;
-                		input>>p.deg;
-                		int temp = p.deg;
-                		cout<<"Enter the Coefficient"<<endl;
-                        for (int i = 0; i <=p.deg; i++)
-                        {
-                       		input>>p.arr[i].co;
-                       		p.arr[i].power = temp; //storing the degree value
-                       		temp--;
-                        }
-                        return input; 
-        }
-        friend ostream & operator<<(ostream &output , poly &p)
-        {
-                	output<<"The entered polynomial is: "<<endl;
-                	int val = p.deg;
-                	for (int i = 0; i <=p.deg; ++i)
-                	{
-                		if(i == p.deg)
-                		{
-                			output<<p.arr[i].co;
-                		}
-                		else
-                			output<<p.arr[i].co<<"x^"<<p.arr[i].power<<"+";
-                	}
-                	cout<<endl;
-                	return output;
-        }
-        void add(poly p1)
-        {
-            p temp[10]; //to store the total power and coefficients 
-            p *highestptr , *otherptr; //to point to the highest degree index
-            int highestDegree,otherDegree;
+	int p[MAX];
+	int deg;
+	int x;
+public:
+	poly()
+	{
+		for (int i = 0; i < deg; ++i)
+			p[i] = 0;
+	}
+	void acceptPoly()
+	{
+		cout<<"Enter the degree: "<<endl;
+		cin>>deg;
+		cout<<"Enter the coefficient: "<<endl;
+		for (int i = 0; i <= deg; i++)
+		{
+			cout<<"Enter for deg: "<<i<<endl;
+			cin>>p[i];
+		}
+	}
 
-            if (deg > p1.deg)
-          	{
-        		otherDegree = p1.deg;
-             	highestDegree = deg;
-                highestptr = this->arr;
-             	otherptr = p1.arr;	
-          	}else
-        	{
-             	otherDegree = deg;
-                highestDegree = p1.deg;
-                highestptr = p1.arr;
-                otherptr = this->arr;
-            }
-	            cout<<"adjusting the array coefficients"<<endl;
-	            p s[highestDegree];
-	            int a = 0;
-	            for(int i = highestDegree ; i>=0 ; i--)
-	            {
-	            	if( a <= otherDegree){
-	            		s[i].co = otherptr[a].co;
-	            		s[i].power = otherptr[a].power;
-	            	}
-	            	else{
-	            		s[i].co = 0;
-	            		s[i].power = otherptr[a].power;
-	            	}
-	            	a++;
-	            }
-        
-          
-            for (int i = 0; i <= highestDegree; i++)
-            {
-				cout<<"Checking if"<<highestptr[i].co<<"=="<<s[i].co<<endl;
-				{
-					temp[i].co = highestptr[i].co + s[i].co;
-					temp[i].power = highestptr[i].power;
-					cout<<"The value inserted at temp [ "<<i<<"] is = "<<temp[i].co<<endl;
-					cout<<highestptr[i].co<<"+"<<otherptr[i].co<<"="<<temp[i].co<<" power : "<<temp[i].power<<endl;
-				}
-            }
-	        cout<<"Termination of the addition code !! ******"<<endl;
-	               	
-	      	//cout<<"The complete addition in polynomial format is : "<<endl;
-	      	int t = highestDegree ;
-	       	cout<<"Code check: highestDegree value is: "<<highestDegree;
-	       	int result=0,x=0;
-	       cout<<"Enter the value of x: "<<endl;
-	       cin>>x;
-	        for(int i = 0; i <= highestDegree; ++i)
-	        {
-	         if(i == highestDegree)
-	         {
-	            cout<<temp[i].co;
-	            result += temp[i].co;
-	         }
-	         else{
-	            cout<<temp[i].co<<"x^"<<temp[i].power<<"+"; 
-	            result += temp[i].co*(pow(x,temp[i].power));
-	            }  		
-	        }
+	void displayPoly()
+	{
+		cout<<"The entered polynomial is: "<<endl;
+		for (int i = deg; i >= 0; i--)
+		{
+			if(i == 0)
+			{
+				cout<<p[i];
+			}else
+				cout<<p[i]<<"x^"<<i<<"+";
+		}
+	}
 
-	        cout<<"The addition of polynomial is: "<<result;
-	    }           
+	void evalPoly()
+	{
+		int x;
+		cout<<"Evaluation of polynomial: "<<endl;
+		cout<<"Enter the value of x : "<<endl;
+		cin>>x;
+		float val = 0;
+		for (int i = deg; i >= 0 ; i--)
+		{
+			if (i == 0)
+			{
+				val = val + p[i];	
+			}
+			else
+				val = val + pow(p[i],i);
+		}
+
+		displayPoly();
+		cout<<"The evaluation of poly: "<<endl;
+		cout<<val<<endl;
+	}
+
+
+	void add(poly &p1)
+	{
+		int highestD = max(deg,p1.deg);
+		int res[highestD];
+
+		for (int i = 0; i <=highestD; ++i)
+			res[i] = 0;
+
+		for (int i = 0; i <= highestD; ++i)
+		{
+			res[i] = p[i]+p1.p[i]; //adding to the resultant array
+		}
+
+		cout<<"The result is: "<<endl;
+		//displaying the result ..
+		for (int i = highestD; i >= 0; i--)
+		{
+			if(i == 0)
+			{
+				cout<<res[i];
+			}else
+				cout<<res[i]<<"x^"<<i<<"+";
+		}
+		//then u can evalute all the cal urself !
+		//cout<<"Enter the value of X"<<endl;
+	}
+
 };
 
-int main(){
+
+int main()
+{
 	poly p1,p2;
-	cin>>p1;
-	cout<<p1;
-	cin>>p2;
-	cout<<p2;
-	p1.add(p2);
+	p1.acceptPoly();
+	p1.displayPoly();
+	p2.acceptPoly();
+	p2.displayPoly();
+	p1.add(p2); //function to be added ! 
 	return 0;
 }
